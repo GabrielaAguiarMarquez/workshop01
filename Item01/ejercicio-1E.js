1c01318ee1963fbdb8926ee351f59f39f9017f44
Gabriela Aguiar <gabrielaaguiarmarquez@gmail.com>
2023-12-16 19:25:58 -0500

    Correccion al ejercicio 1E.

--- Item01/ejercicio-1E.js
+++ Item01/ejercicio-1E.js
@@ -1,9 +1,9 @@
 // Hacer un algoritmo con JavaScript que reciba cualquier letra del abecedario y muestre en consola el valor booleano que indique si es o no una vocal.
 
-const letra = prompt("Ingresa una letra");
+const letra = prompt("Ingresa una letra").toLowerCase;
 
 let vocal = letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u";
 
-console.log(`La letra ingresada es una vocal: ${vocal}`)
+console.log(`La letra ingresada es una vocal: ${vocal}`);
 
 
