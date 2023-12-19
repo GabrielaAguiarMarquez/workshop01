ac4a8fad257fd1622e447664e9544a5f49773b63
Gabriela Aguiar <gabrielaaguiarmarquez@gmail.com>
2023-12-19 13:58:21 -0500

    Correccion de la función .toLowerCase (agregar parentesis)

--- Item01/ejercicio-1E.js
+++ Item01/ejercicio-1E.js
@@ -1,6 +1,6 @@
 // Hacer un algoritmo con JavaScript que reciba cualquier letra del abecedario y muestre en consola el valor booleano que indique si es o no una vocal.
 
-const letra = prompt("Ingresa una letra").toLowerCase;
+const letra = prompt("Ingresa una letra").toLowerCase();
 
 let vocal = letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u";
 
